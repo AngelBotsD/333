fcfc4e3c110bec28a87fb04252cc88f4d1776a33
AngelBotsD <angelperez22448@gmail.com>
2025-10-05 11:02:11 -0600

    Actualizar group-hidetag.js

--- plugins/group-hidetag.js
+++ plugins/group-hidetag.js
@@ -16,7 +16,7 @@ const handler = async (m, { conn, participants }) => {
         },
         message: {
             locationMessage: {
-                name: `𝖧𝗈𝗅𝖺, 𝖲𝗈𝗒 𝖡𝖺𝗄𝗂-𝖡𝗈𝗍`,
+                name: `𝖧𝗈𝗅𝖺, 𝖲𝗈𝗒 333-𝖡𝗈𝗍`,
                 jpegThumbnail: thumb
             }
         },
@@ -53,19 +53,19 @@ const handler = async (m, { conn, participants }) => {
 
                     if (finalText) {
                         await conn.sendMessage(m.chat, {
-                            text: `${finalText}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                            text: `${finalText}\n\n> 333 - 𝙱𝙾𝚃`,
                             mentions: users
                         }, { quoted: fkontak })
                     }
                 } catch {
                     await conn.sendMessage(m.chat, {
-                        text: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                        text: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`,
                         mentions: users
                     }, { quoted: fkontak })
                 }
             } else {
-                if (mtype === 'imageMessage') await conn.sendMessage(m.chat, { image: media, caption: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`, mentions: users }, { quoted: fkontak })
-                if (mtype === 'videoMessage') await conn.sendMessage(m.chat, { video: media, caption: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`, mentions: users, mimetype: 'video/mp4' }, { quoted: fkontak })
+                if (mtype === 'imageMessage') await conn.sendMessage(m.chat, { image: media, caption: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`, mentions: users }, { quoted: fkontak })
+                if (mtype === 'videoMessage') await conn.sendMessage(m.chat, { video: media, caption: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`, mentions: users, mimetype: 'video/mp4' }, { quoted: fkontak })
                 if (mtype === 'stickerMessage') await conn.sendMessage(m.chat, { sticker: media, mentions: users }, { quoted: fkontak })
             }
         } else if (m.quoted && !isMedia) {
@@ -76,7 +76,7 @@ const handler = async (m, { conn, participants }) => {
                     { [mtype || 'extendedTextMessage']: q.message?.[mtype] || { text: finalCaption } },
                     { quoted: fkontak, userJid: conn.user.id }
                 ),
-                `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`,
                 conn.user.jid,
                 { mentions: users }
             )
@@ -94,24 +94,24 @@ const handler = async (m, { conn, participants }) => {
 
                     if (finalText) {
                         await conn.sendMessage(m.chat, {
-                            text: `${finalText}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                            text: `${finalText}\n\n> 333 - 𝙱𝙾𝚃`,
                             mentions: users
                         }, { quoted: fkontak })
                     }
                 } catch {
                     await conn.sendMessage(m.chat, {
-                        text: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                        text: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`,
                         mentions: users
                     }, { quoted: fkontak })
                 }
             } else {
-                if (mtype === 'imageMessage') await conn.sendMessage(m.chat, { image: media, caption: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`, mentions: users }, { quoted: fkontak })
-                if (mtype === 'videoMessage') await conn.sendMessage(m.chat, { video: media, caption: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`, mentions: users, mimetype: 'video/mp4' }, { quoted: fkontak })
+                if (mtype === 'imageMessage') await conn.sendMessage(m.chat, { image: media, caption: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`, mentions: users }, { quoted: fkontak })
+                if (mtype === 'videoMessage') await conn.sendMessage(m.chat, { video: media, caption: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`, mentions: users, mimetype: 'video/mp4' }, { quoted: fkontak })
                 if (mtype === 'stickerMessage') await conn.sendMessage(m.chat, { sticker: media, mentions: users }, { quoted: fkontak })
             }
         } else {
             await conn.sendMessage(m.chat, {
-                text: `${finalCaption}\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+                text: `${finalCaption}\n\n> 333 - 𝙱𝙾𝚃`,
                 mentions: users
             }, { quoted: fkontak })
         }
@@ -119,7 +119,7 @@ const handler = async (m, { conn, participants }) => {
     } catch (e) {
         const users = participants.map(u => conn.decodeJid(u.id))
         await conn.sendMessage(m.chat, {
-            text: `🔊 Notificación\n\n> 𝙱𝙰𝙺𝙸 - 𝙱𝙾𝚃`,
+            text: `🔊 Notificación\n\n> 333 - 𝙱𝙾𝚃`,
             mentions: users
         }, { quoted: fkontak })
     }
